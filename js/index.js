9baabbbe31d88c8f4da61810edd57c6e9916d6f4
AlekseyMor0z <alexiusmoroz@gmail.com>
2023-04-23 00:12:07 +0300

    Add first section in main, index.html

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const btnsAboutCompany = document.querySelectorAll(".btn-about-company");
+const descriptionAboutCompany = document.querySelector(".description-about-company");
+
+btnsAboutCompany.forEach(element => {
+    element.addEventListener('click', () => {
+        
+    })
+});
